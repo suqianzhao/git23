95015fe984f179be573519d42f6a43e0a879ef87
sqz <1226080657@qq.com>
2020-10-10 10:55:12 +0800

    add sqz

--- src/test/java/gitgit/gitgit/Helloword.java
+++ src/test/java/gitgit/gitgit/Helloword.java
@@ -3,6 +3,13 @@ package gitgit.gitgit;
 public class Helloword {
    public static void main(String[]args){
 	   System.out.println("hello word");
+	   System.out.println("hello sqz");
+	   System.out.println("hello sqz");
+	   System.out.println("hello sqz");
+	   System.out.println("hello sqz");
+	   System.out.println("hello sqz");
+	   System.out.println("hello sqz");
+	   
 	   }
    
 }
